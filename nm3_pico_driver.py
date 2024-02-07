02ff7457843a0df4b590209517adf7b29a139056
DeanP-R <Deanrowlett00@gmail.com>
2024-02-07 14:38:07 +0000

    Added Thonny & mPython versions

--- nm3_pico_driver.py
+++ nm3_pico_driver.py
@@ -4,7 +4,7 @@
 # Description   : Driver for interfacing with the NanoModem V3.2 acoustic modem on Raspberry Pi Pico W. #
 # Author        : Dean Rowlett                                                                          #
 # Target        : Raspberry Pi Pico W with MicroPython                                                  #
-# Interpreter   : MicroPython                                                                           #
-# IDE           : Thonny                                                                                #
+# Interpreter   : MicroPython v1.22.1                                                                   #
+# IDE           : Thonny 4.1.4                                                                          #
 # Last Updated  : 7th February 2024                                                                     #
 #########################################################################################################
