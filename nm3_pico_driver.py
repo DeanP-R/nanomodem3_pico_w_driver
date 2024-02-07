ac05f38c3f806e35f0ff854d3c9ee12c0f227bb8
DeanP-R <75754203+DeanP-R@users.noreply.github.com>
2024-02-07 14:33:54 +0000

    Create nm3_pico_driver.py

---
+++ nm3_pico_driver.py
@@ -0,0 +1,10 @@
+#########################################################################################################
+# Filename      : nm3_pico_driver.py                                                                    #
+# Version       : 0.1.0                                                                                 #
+# Description   : Driver for interfacing with the NanoModem V3.2 acoustic modem on Raspberry Pi Pico W. #
+# Author        : Dean Rowlett                                                                          #
+# Target        : Raspberry Pi Pico W with MicroPython                                                  #
+# Interpreter   : MicroPython                                                                           #
+# IDE           : Thonny                                                                                #
+# Last Updated  : 7th February 2024                                                                     #
+#########################################################################################################
