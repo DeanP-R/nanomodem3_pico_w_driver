bce432580d44f499f9cf073128c3f270c2ba7176
DeanP-R <deanrowlett00@gmail.com>
2024-02-11 15:13:15 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#########################################################################################################
+# Filename      : nmain.py                                                                              #
+# Version       : 0.1.0                                                                                 #
+# Description   : Main program for handling communications for MEng ROpeless Fishing Gear Demonstrator. #
+# Author        : Dean Rowlett                                                                          #
+# Target        : Raspberry Pi Pico W with MicroPython                                                  #
+# Interpreter   : MicroPython v1.22.1                                                                   #
+# IDE           : Thonny 4.1.4                                                                          #
+# Last Updated  : 11th February 2024                                                                    #
+#########################################################################################################
+
