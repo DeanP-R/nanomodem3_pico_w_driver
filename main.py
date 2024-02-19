79d76c7211dfbe03e607746ccf68a8efe810ab7a
DeanP-R <deanrowlett00@gmail.com>
2024-02-19 14:15:33 +0000

    servo testing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #########################################################################################################
-# Filename      : nmain.py                                                                              #
+# Filename      : main.py                                                                               #
 # Version       : 0.0.1                                                                                 #
 # Description   : Main program for handling communications for MEng Ropeless Fishing Gear Demonstrator. #
 # Author        : Dean Rowlett                                                                          #
@@ -8,20 +8,38 @@
 # IDE           : Thonny 4.1.4                                                                          #
 # Last Updated  : 11th February 2024                                                                    #
 #########################################################################################################
-
+"""
 from nm3_pico_driver import NM3Driver
-message = "TEST"
+message = "Ocean Lab Test"
 modem = 169
 
 pico = NM3Driver()
 pico.connect()
-addr = pico.get_address()
-print(f"Address: {addr}")
+#addr = pico.get_address()
+#print(f"Address: {addr}")
 
 voltage = pico.get_voltage()
 print(f"Voltage: {voltage}")
-distance = pico.ping(169)
+distance = str(pico.ping(modem))
 
 print(f"Distance to modem 169: {distance}")
-send_test = pico.send_unicast_message(modem, message)
+send_test = pico.send_unicast_message(modem, distance)
 print(f"Sending message: {message}. To modem: {modem}. Returning: {send_test}.")
+"""
+class Servo:
+    def __init__(self, MIN_DUTY=300000, MAX_DUTY=2300000, pin=27, freq=50):
+        self.pwm = machine.PWM(machine.Pin(pin))
+        self.pwm.freq(freq)
+        self.MIN_DUTY = MIN_DUTY
+        self.MAX_DUTY = MAX_DUTY
+        
+    def rotateDeg(self, deg):
+        if deg < 0:
+            deg = 0
+        elif deg > 180:
+            deg = 180
+        duty_ns = int(self.MAX_DUTY - deg * (self.MAX_DUTY-self.MIN_DUTY)/180)
+        self.pwm.duty_ns(duty_ns)
+
+servo = Servo()
+servo.rotateDeg(90)
