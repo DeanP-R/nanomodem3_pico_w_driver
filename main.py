0b4a693a222b9be6e48ae7d3e3516178434a64c7
DeanP-R <deanrowlett00@gmail.com>
2024-03-13 11:59:52 +0000

    Added Deep sleep test

--- main.py
+++ main.py
@@ -12,6 +12,8 @@
 #########################################################################################################
 from nm3_pico_driver import NM3Driver
 from servo import Servo
+import machine
+import utime
 
 servo_pin = 26
 servo_modem = 160
@@ -22,15 +24,24 @@ pico.connect()
 
 servo = Servo(servo_pin)
 
-
-while True:
-
+# Function to process incoming responses
+def process_response():
     response = pico.read_response().strip()  # Strip whitespace from response
-    print(f"Response: '{response}'")  
+    print(f"Response: '{response}'")
     if response == '#U07RELEASE':
         servo.move(180)
     elif response:  # Only attempt to lock if there's a non-empty response
         servo.move(90)
-    else:
-        continue
+
+# Active phase: 9 hours
+# Convert 9 hours into seconds for the loop
+nine_hours_in_seconds = 9 * 60 * 60
+start_time = utime.time()
+while utime.time() - start_time < nine_hours_in_seconds:
+    process_response()
+    utime.sleep(1)  # Sleep 1 second between readings to reduce power consumption, not sure if this will effect functionality
+
+# After the active phase, go into deep sleep for 15 hours
+# 15 hours = 54,000,000 milliseconds
+machine.deepsleep(54000000)
         
